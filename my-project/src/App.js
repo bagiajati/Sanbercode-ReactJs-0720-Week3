573c37ee3d068ac7867ced2fafdc5c43b6d2cd76
bagiajati <bagiajati@gmail.com>
2020-08-04 06:36:12 +0700

    Menambahkan file DataHargaBuah.js, Tugas11.js dan Style11.module.css dalam folder tugas11

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tugas11 from './tugas11/Tugas11'
+import Style11 from "./tugas11/Style11.module.css"
 
+
+// Tugas 11
 function App() {
+  const thead = {
+    cellSpacing: "0px",
+    border: "1 px solid #000000",
+    borderSpacing: "5px",
+    backgroundColor: "#808080",
+    textAlign: "center",
+    padding: "2px"
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{textAlign: "center", fontFamily: "Times New Roman"}}>Tabel Harga Buah</h1>
+      <table className="thead" style={thead}>
+      <tr>
+        <td><b>Nama</b></td>
+        <td><b>Harga</b></td>
+        <td><b>Berat</b></td>
+      </tr>
+      </table>
+      < Tugas11 />
     </div>
   );
 }
